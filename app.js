fd87a9007501d57cce54df82a85575920bc94ff0
Nancy <duong.nancy@gmail.com>
2025-05-08 14:57:24 -0700

    Added JS functionality

---
+++ app.js
@@ -0,0 +1,133 @@
+// ****** Select Items *******
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// ******* Event Listeners *******
+// submit form
+form.addEventListener('submit', addItem)
+
+// clear items
+clearBtn.addEventListener('click', clearItems);
+
+
+
+// ******* Functions *******
+function addItem(e) {
+    e.preventDefault();
+    //console.log(grocery.value);
+    const value = grocery.value;
+    // if (value) {
+        //console.log("value is truthy");
+    //}
+    //if (!value) {
+        //console.log("value is falsy");
+    //}
+    const id = new Date().getTime().toString();
+    //console.log(id);
+    //if (value !== "" && editFlag == false) {
+    //}
+    if (value && !editFlag) {
+        //console.log("add item to the list");
+    //else if (value !== "" && editFlag == true) {
+        //}
+        const element = document.createElement("article");
+        // add class
+        element.classList.add("grocery-item");
+        // add id
+        const attribute = document.createAttribute("data-id");
+        attribute.value = id;
+        element.setAttributeNode(attribute);
+        element.innerHTML = `<p class="title">${value}</p>
+                    <div class="btn-container">
+                        <button type="button" class="edit-btn">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                        <button type="button" class="delete-btn">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </div>`;
+        const deleteBtn = element.querySelector(".delete-btn");
+        const editBtn = element.querySelector(".edit-btn");
+        deleteBtn.addEventListener('click', deleteItem);
+        editBtn.addEventListener('click', editItem);
+        
+        // append child
+        list.appendChild(element);
+        // display alert
+        displayAlert("item added to the list", "success");
+        // show container
+        container.classList.add("show-container");
+        // add to local storage
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault();
+    } else if (value && editFlag) {
+       // console.log("editing");
+    } else {
+        //console.log("empty value");
+        //alert.textContent = "empty value";
+        //alert.classList.add("alert-danger");
+        displayAlert("please enter value", "danger");  
+    }
+}
+
+// display alert
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    // remove alert
+    setTimeout(function () {
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 1000);
+}
+
+// clear items
+function clearItems() {
+    const items = document.querySelectorAll(".grocery-item");
+
+    if (items.length > 0) {
+        items.forEach(function (item) {
+            list.removeChild(item)
+        });
+    }
+    container.classList.remove("show-container");
+    displayAlert("empty list", "danger");
+    setBackToDefault();
+    // localStorage.removeItem("list");
+}
+
+
+// delete function
+function deleteItem() {
+    console.log("item deleted");
+}
+// edit function
+function editItem() {
+    console.log("edit item");
+}
+// set back to default
+function setBackToDefault() {
+    //console.log("set back to default");
+    grocery.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = "submit";
+}
+// ******* Local Storage *******
+function addToLocalStorage(id, value) {
+    console.log("added to local storage");
+}
+// ******* Setup Items ********
+
